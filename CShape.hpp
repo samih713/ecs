6c5587e74da14ca9fc970de21468785359ee4b61
Samih <sakmohamed@gmail.com>
2025-10-01 18:32:20 +0400

    feat[CShape]: remove redundant lines

--- CShape.hpp
+++ CShape.hpp
@@ -13,20 +13,19 @@ struct CShape
         {
             throw std::runtime_error("Invalid texture");
         }
-        float width = sprite.width;
-        float height = sprite.height;
+
         src = Rectangle{
             .x = 0,
             .y = 0,
-            .width = width,
-            .height = height,
+            .width = (float)sprite.width,
+            .height = (float)sprite.height,
         };
+
         origin = Vector2{
             .x = dst.width / 2,
             .y = dst.height / 2,
         };
     }
-    CShape(const CShape &o) = default;
     ~CShape() { UnloadTexture(sprite); }
     Rectangle src, dst;
     Vector2 origin;
